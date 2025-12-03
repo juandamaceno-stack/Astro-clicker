eaf2232242af21d627a0f87be09fcd48efc4daee
Juan Pablo Gs <juan_damaceno@estudante.sesisenai.org.br>
2025-12-03 16:00:01 -0300

    segunda atualização do menu

---
+++ Assets/Scripts/SairConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SairConfig : MonoBehaviour
+{
+   public void IniciarJogo()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
+
