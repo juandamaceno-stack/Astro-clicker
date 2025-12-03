3cd8f09907c78782a6dae9fe84e8ede756876bbb
Juan Pablo Gs <juan_damaceno@estudante.sesisenai.org.br>
2025-12-03 14:20:15 -0300

    primeiro commit

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -20,7 +20,7 @@ public class SpawnObject : MonoBehaviour
     void Update(){
 
         // Verifica se a posição do objeto instanciado está fora da câmera
-        if(transform.position.y < -7){
+        if(transform.position.y < -7 || transform.position.y > 7f){
 
             // Se sim, destrói o objeto
             Destroy(gameObject);
